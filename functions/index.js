5462d3ed1976c9ff6965cbf7399057cdb00c261d
Fanessa <fanessah@gmail.com>
2022-11-21 09:04:22 -0500

    created login

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ import functions from "firebase-functions"
 import express from 'express'
 import cors from 'cors'
 import { getAllPhotos,addNewPhotos } from "./src/photos.js";
+import { userLogin,addNewUser,updateUser} from "./src/users.js";
 
 const app = express();
 app.use(cors());
@@ -10,7 +11,9 @@ app.use(express.json());
 // routes here 
 app.get('/photos', getAllPhotos) // get all photos 
 app.post('/photos', addNewPhotos) // add new photos
-
+app.post('/login', userLogin)
+app.post('/users',addNewUser)
+app.patch('users/:userId',updateUser)
 
 
 
--- functions/src/photos.js
+++ functions/src/photos.js
@@ -1,18 +1,23 @@
-import dbConnect from "./dbConnect.js";
+
+
+import dbConnect from "./dbconnect.js";
+
 
 export async function getAllPhotos(req, res) {
     const db = dbConnect();
     const collection = await db.collection('photos').get()
-    const photos = collection.docs.map(doc => ({...doc.data(), photoId: doc.id }));
-res.send(photos)
+        .catch(err => res.status(500).send(err));
+    const photos = collection.docs.map(doc => ({ ...doc.data(), photoId: doc.id }));
+    res.send(photos);
 }
 
 export async function addNewPhotos(req, res) {
     const newPhoto = req.body;
     const db = dbConnect()
-     await db.collection('photos').add(newPhoto)
-     .catch(err => res.status(500).send(err));
-     getAllPhotos(req,res);
-    
+    await db.collection('photos').add(newPhoto)
+        .catch(err => res.status(500).send(err));
+    getAllPhotos(req, res);
+
 
 }
+
